4e6727bae7ddae7742b2500e7013f8612fba8df6
RaphaelusEthaneus <caitanjustin9211@gmail.com>
2024-07-04 01:17:17 +0800

    .

---
+++ Project/PRODUCT PAGE/MAIN-PROUDCT.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var carouselContainers = document.querySelectorAll('.carousel-container');
+
+    carouselContainers.forEach(function(container) {
+        var carousel = container.querySelector('.product-carousel');
+        var prevButton = container.querySelector('.carousel-prev');
+        var nextButton = container.querySelector('.carousel-next');
+
+        var currentSlide = 0;
+        var totalSlides = carousel.querySelectorAll('.product-item').length;
+
+        showSlide(currentSlide);
+
+        prevButton.addEventListener('click', function() {
+            currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+            showSlide(currentSlide);
+        });
+
+        nextButton.addEventListener('click', function() {
+            currentSlide = (currentSlide + 1) % totalSlides;
+            showSlide(currentSlide);
+        });
+
+        function showSlide(slideIndex) {
+            var slides = carousel.querySelectorAll('.product-item');
+
+            slides.forEach(function(slide) {
+                slide.style.display = 'none';
+            });
+
+            slides[slideIndex].style.display = 'block';
+
+            carousel.style.height = slides[slideIndex].offsetHeight + 'px';
+        }
+    });
+});
